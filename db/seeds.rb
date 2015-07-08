58b7d53a71df87adb1c382ddc88fd4c2c521d8d3
Francisco Limongi <zico@ri5.com.br>
2015-07-08 20:21:57 -0300

    melhorando o seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@ admin = Admin.new
 admin.email = 'zicolimongi@gmail.com'
 admin.password = 'guitarra'
 admin.save
+
+20.times do
+  Product.create(
+      name: "Tênis Preto Nike",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, deleniti cupiditate autem. Quo provident quos, labore ducimus, quam doloribus alias repudiandae placeat, sunt illum voluptatibus veniam est velit commodi, facere.",
+      image_link: "http://g04.a.alicdn.com/kf/HTB1VomoIpXXXXXfXVXXq6xXFXXXM/Sneakers-mulheres-2015-homens-Sneakers-cal%C3%A7ados-esportivos-homens-huarache-zapatos-homens-sapatos-de-cunha-t%C3%AAnis-running.jpg_200x200.jpg",
+      price: 199)
+end
